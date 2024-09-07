4ab45a7f3cafac8f350c8822cffacec5dee7e8f8
sunil-kumar <skmy03@gmail.com>
2024-09-07 19:09:07 +0530

    Fixed redis client, add retry mechanism with delay, fixed auth issue

--- app/models/pydantic/product.py
+++ app/models/pydantic/product.py
@@ -6,12 +6,3 @@ class ProductModel(BaseModel):
     product_title: str = Field(..., title="Product Title", max_length=255)
     product_price: float = Field(..., gt=0, title="Product Price")
     path_to_image: Optional[str] = Field(None, title="Path to Image")
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "product_title": "Example Product",
-                "product_price": 199.99,
-                "path_to_image": "/images/product1.jpg",
-            }
-        }
--- app/services/database.py
+++ app/services/database.py
@@ -9,15 +9,15 @@ class DatabaseClient:
         if not os.path.exists(self.file_path):
             self._initialize_db()
 
-        def _initialize_db(self):
-            with open(self.file_path, "w") as db_file:
-                json.dump([], db_file)
+    def _initialize_db(self):
+        with open(self.file_path, "w") as db_file:
+            json.dump([], db_file)
 
-        def load_data(self):
-            with open(self.file_path, "r") as db_file:
-                data = json.load(db_file)
-                return [ProductModel(**item) for item in data]
+    def load_data(self):
+        with open(self.file_path, "r") as db_file:
+            data = json.load(db_file)
+            return [ProductModel(**item) for item in data]
 
-        def save_data(self, data):
-            with open(self.file_path, "w") as f:
-                json.dump([item.dict() for item in data], f, indent=4)
+    def save_data(self, data):
+        with open(self.file_path, "w") as f:
+            json.dump([item.dict() for item in data], f, indent=4)
